10924525f0a9346e50591bd79454f2ca70f7cbaf
João <ut7vitor60@gmail.com>
2025-01-03 20:04:04 -0300

    Criação do ambiente react, Criação do componente banner

---
+++ organo/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Banner from './componentes/Banner/Banner.jsx'
+
+function App() {
+  
+  return (
+    
+  <Banner/>
+  )
+}
+
+export default App
---
+++ organo/src/componentes/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner (){
+    return(
+        <header className='banner_principal'>
+             <img src="/banner.png" alt="Banner Principal da Página"/>
+        </header>
+   
+    )
+   
+}
+
+export default Banner;
