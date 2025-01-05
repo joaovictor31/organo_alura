eba297d3748ece13ab5274c4837ba4674d74e23b
João <ut7vitor60@gmail.com>
2025-01-05 19:47:31 -0300

    Adicionando componente lista suspensa e suas opções

--- organo/src/componentes/Formulário/Formulário.jsx
+++ organo/src/componentes/Formulário/Formulário.jsx
@@ -1,7 +1,11 @@
 import './Formulário.css'
+import ListaSuspensa from '../Lista Suspensa/ListaSuspensa';
+
 import Dados from './Dados';
 
 function Formulario(){
+    const times = ['Programação', 'Front-End','Data Science', 'Devops','UX e Design','Mobile', 'Inovação e Gestão'];
+
     return(
         <div className="card_colaborador">
     <h2 className="titulo">Preencha os dados para criar o card do colaborador</h2>
@@ -9,7 +13,8 @@ function Formulario(){
      <Dados label = 'Nome' placeholder = 'Digite seu nome'/>
      <Dados label = 'Cargo' placeholder = 'Digite seu cargo'/>
      <Dados label = 'Imagem' placeholder = 'Informe o endereço da imagem'/>
-    
+     <ListaSuspensa label = 'Time 'itens = {times}/>
+
     </div>
 </div>
     )
